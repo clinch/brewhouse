564850df48f621b7881d4b483a5fa696dc36d01c
Chris Inch <github@chrisinch.com>
2015-10-20 21:23:54 -0400

    Added an initial test case

---
+++ test/Server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+describe('Server', function() {
+  describe('init', function () {
+    it('should return true, always', function () {
+      assert.equal(true, true);
+    });
+  });
+});

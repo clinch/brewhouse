71deb9c9871231e880974a1b2eaf22fd6025a526
Chris Inch <github@chrisinch.com>
2015-10-21 22:02:40 -0400

    Some basic express and router info

--- app/Server.js
+++ app/Server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let constants = require('./constants');
 
 let debug = require('debug')(`${constants.APP_NAME}:Server`);
@@ -11,6 +14,33 @@ class Server {
 
 	init() {
 		debug('Server is initializing.');
+
+		this.app = express();
+
+		// configure app to use bodyParser()
+		// this will let us get the data from a POST
+		this.app.use(bodyParser.urlencoded({ extended: true }));
+		this.app.use(bodyParser.json());
+
+		this.port = process.env.PORT || 8080;
+
+		// ROUTES FOR OUR API
+		this.router = express.Router();
+		
+		// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+		this.router.get('/', function(req, res) {
+		    res.json({ message: 'hooray! welcome to our api!' });   
+		});
+
+		// more routes for our API will happen here
+
+		// REGISTER OUR ROUTES
+		// all of our routes will be prefixed with /api
+		this.app.use('/api', this.router);
+
+		// START THE SERVER
+		this.app.listen(this.port);
+		console.log('Magic happens on port ' + this.port);
 	}
 }
 
--- test/Server.js
+++ test/Server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var assert = require('assert');
+
 describe('Server', function() {
   describe('init', function () {
     it('should return true, always', function () {

c7602e7e01ec1b7ae6e2930b004b4007f2309eca
Chris Inch <github@chrisinch.com>
2015-10-22 20:22:02 -0400

    Added nconf and a default config file.

--- app/Server.js
+++ app/Server.js
@@ -2,6 +2,8 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
+let fs = require('fs');
+let nconf = require('nconf');
 
 let constants = require('./constants');
 
@@ -12,24 +14,34 @@ class Server {
 		
 	}
 
+	/**
+	 * Called once at the very start of this app. Initializes all components and
+	 * starts the server.
+	 */
 	init() {
 		debug('Server is initializing.');
 
 		this.app = express();
+
+		this.initConfig();
 
 		// configure app to use bodyParser()
 		// this will let us get the data from a POST
 		this.app.use(bodyParser.urlencoded({ extended: true }));
 		this.app.use(bodyParser.json());
 
-		this.port = process.env.PORT || 8080;
+		this.port = nconf.get('port') || 8080;
 
 		// ROUTES FOR OUR API
 		this.router = express.Router();
 		
-		// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+		// Default route will output information that will be useful to API consumers
 		this.router.get('/', function(req, res) {
-		    res.json({ message: 'hooray! welcome to our api!' });   
+				// npm_package_version is set as an env variable when using 'npm start'
+				if (!nconf.get('npm_package_version')) {
+					throw new Error(`No npm version available. Launch server with 'npm start'`);
+				}
+		    res.json({ version: nconf.get('npm_package_version') });   
 		});
 
 		// more routes for our API will happen here
@@ -40,7 +52,16 @@ class Server {
 
 		// START THE SERVER
 		this.app.listen(this.port);
-		console.log('Magic happens on port ' + this.port);
+		console.log(`The server is now up and running on port ${this.port}`);
+	}
+
+	/**
+	 * Initializes any config file options and specifies config file location
+	 */
+	initConfig() {
+		nconf.argv()
+   		.env()
+   		.file({ file: './config/config.json' });
 	}
 }
 
